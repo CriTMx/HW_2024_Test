7dddbe21a55c412fe70a8aa3ea0523a02226d6e6
CriTMx <savefailedmc@gmail.com>
2024-08-25 21:30:18 +0530

    implemented json data fetching from server

--- Assets/Scripts/FetchGameData.cs
+++ Assets/Scripts/FetchGameData.cs
@@ -10,6 +10,9 @@ public class FetchGameData : MonoBehaviour
     [SerializeField]
     private string serverURL = "https://s3.ap-south-1.amazonaws.com/superstars.assetbundles.testbuild/doofus_game/doofus_diary.json";
 
+    public static float PlayerSpeed;
+    public static float MinPulpitDestroyTime, MaxPulpitDestroyTime, PulpitSpawnTime;
+
     void Start()
     {
         StartCoroutine(GetGameDataFromServer());
@@ -23,17 +26,43 @@ public class FetchGameData : MonoBehaviour
 
     IEnumerator GetGameDataFromServer()
     {
-        using (UnityWebRequest server = UnityWebRequest.Get(serverURL))
-        {
-            yield return server.Send();
+        using UnityWebRequest server = UnityWebRequest.Get(serverURL); // Establish connection to the server
+        yield return server.SendWebRequest(); 
 
-            if (server.isNetworkError || server.isHttpError)
-                Debug.Log(server.error);
-            else
-            {
-                dataObject = server.downloadHandler.text;
-                Debug.Log(dataObject);
-            }
+        if (server.result == UnityWebRequest.Result.ConnectionError || server.result == UnityWebRequest.Result.ProtocolError)
+            Debug.Log(server.error); // Log error if server responds with connection error or protocol error
+
+        else
+        {
+            dataObject = server.downloadHandler.text; // Fetch JSON data from server as a string
+            GameData gameData = JsonUtility.FromJson<GameData>(dataObject);
+
+            // Set game data values to store until Game exits
+            PlayerSpeed = gameData.player_data.speed;
+            MinPulpitDestroyTime = gameData.pulpit_data.min_pulpit_destroy_time;
+            MaxPulpitDestroyTime = gameData.pulpit_data.max_pulpit_destroy_time;
+            PulpitSpawnTime = gameData.pulpit_data.pulpit_spawn_time;
         }
     }
+}
+
+[System.Serializable]
+public class GameData
+{
+    public PlayerData player_data;
+    public PulpitData pulpit_data;
+}
+
+[System.Serializable]
+public class PlayerData
+{
+    public float speed;
+}
+
+[System.Serializable]
+public class PulpitData
+{
+    public float min_pulpit_destroy_time;
+    public float max_pulpit_destroy_time;
+    public float pulpit_spawn_time;
 }
