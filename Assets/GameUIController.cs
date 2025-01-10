f1e3fc051e9e33c624b524ccb6c977d7077d86ce
CriTMx <savefailedmc@gmail.com>
2025-01-10 18:25:07 +0530

    pause screen

---
+++ Assets/GameUIController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameUIController : MonoBehaviour
+{
+
+    // TMP GUI components
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+
+    [SerializeField] private GameObject pauseScreen;
+    [SerializeField] private TextMeshProUGUI pauseText;
+
+    [SerializeField] private GameObject scoreboard;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private void Awake()
+    {
+        PlayerDeath.OnPlayerDeath += ShowGameOverScreen;
+
+        // Disable game over screen at the start of the game
+        gameOverScreen.SetActive(false);
+    }
+
+
+    void Start()
+    {
+        // Initialize scoreboard text
+        scoreText.text = "Score\n" + PlayerScore.GetScore().ToString();
+        timeText.text = "Time\n" + PlayerScore.GetGameTime().ToString("#.#") + "s";
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score\n" + PlayerScore.GetScore().ToString();
+        timeText.text = "Time\n" + PlayerScore.GetGameTime().ToString("#.#") + "s";
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePauseScreen();
+        }
+    }
+
+    void ShowGameOverScreen()
+    {
+        /* Summarize score and add it as text 
+         * on the game over screen,
+         * disable scoreboard UI and 
+         * show game over screen */
+
+        string gameOverResultText = 
+            "Game Over!" + "\nScore: " 
+            + PlayerScore.GetScore().ToString() + 
+            "\nTime: " + PlayerScore.GetGameTime().ToString("#.#") + "s";
+
+        gameOverText.text = gameOverResultText;
+        gameOverScreen.SetActive(true);
+        scoreboard.SetActive(false);
+    }
+
+    void TogglePauseScreen()
+    {
+        pauseScreen.SetActive(!pauseScreen.activeSelf);
+
+        if (!pauseScreen.activeSelf)
+        {
+            Time.timeScale = 1f;
+            return;
+        }
+
+        UpdatePauseScreenText();
+        Time.timeScale = 0f;
+    }
+
+    void UpdatePauseScreenText()
+    {
+        pauseText.text =
+            "Game paused" + "\nCurrent Score: "
+            + PlayerScore.GetScore().ToString();
+    }
+
+
+    private void OnDestroy()
+    {
+        PlayerDeath.OnPlayerDeath -= ShowGameOverScreen;
+    }
+
+    private void OnDisable()
+    {
+        PlayerDeath.OnPlayerDeath -= ShowGameOverScreen;
+    }
+
+}
--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -5,41 +5,15 @@ using TMPro;
 
 public class PlayerScore : MonoBehaviour
 {
-    // TMP GUI text components
-    [SerializeField] private GameObject scoreboard;
-    [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private TextMeshProUGUI timeText;
-
-    [SerializeField] private GameObject gameOverScreen;
-    [SerializeField] private TextMeshProUGUI gameOverText;
-
     // To hold player score and game time values for the scoreboard
-    private int playerScore = 0;
-    private float gameTime = 0f;
+    private static int playerScore = 0;
+    private static float gameTime = 0f;
 
-    private void Awake()
-    {
-        PlayerDeath.OnPlayerDeath += ShowGameOverScreen;
-
-        // Disable game over screen at the start of the game
-        gameOverScreen.SetActive(false);
-    }
-
-    void Start()
-    {
-        // Initialize scoreboard text
-        scoreText.text = "Score\n" + playerScore.ToString();
-        timeText.text = "Time\n" + gameTime.ToString("#.#") + "s";
-    }
 
     void Update()
     {
         // Update game time
         gameTime += Time.deltaTime;
-
-        // Update scoreboard text values for player score and time
-        scoreText.text = "Score\n" + playerScore.ToString();
-        timeText.text = "Time\n" + gameTime.ToString("#.#") + "s";
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -56,20 +30,7 @@ public class PlayerScore : MonoBehaviour
         }
     }
 
-    private void OnDestroy()
-    {
-        PlayerDeath.OnPlayerDeath -= ShowGameOverScreen;
-    }
-
-    void ShowGameOverScreen()
-    {
-        /* Summarize score and add it as text 
-         * on the game over screen,
-         * disable scoreboard UI and 
-         * show game over screen */
-
-        gameOverText.text = "Game Over!" + "\nScore: " + playerScore.ToString() + "\nTime: " + gameTime.ToString("#.#")+"s";
-        gameOverScreen.SetActive(true);
-        scoreboard.SetActive(false);
-    }
+    public static int GetScore() => playerScore;
+    public static float GetGameTime() => gameTime;
+ 
 }
